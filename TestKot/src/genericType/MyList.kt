463f41bd53eb404d5060d63963f21c9d50413c30
KMStr <kstrigalev@gmail.com>
2021-05-12 09:52:54 +0300

    Обобщённые типы - Generic

---
+++ TestKot/src/genericType/MyList.kt
@@ -0,0 +1,10 @@
+package genericType
+
+interface MyList<T> {
+
+    fun get(index: Int): T
+    fun add(string: T)
+    fun remove (element: T)
+    fun removeAt (index: Int)
+    fun size (): Int
+}
